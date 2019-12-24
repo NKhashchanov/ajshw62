772d6d1340466ad9494bd6fa77dcc97978e36eca
Николай <37711307+NKhashchanov@users.noreply.github.com>
2019-12-25 01:40:39 +0300

    up

---
+++ src/js/__tests__/app.test.js
@@ -0,0 +1,34 @@
+import Character from '../app';
+
+const exepted1 = {
+  attack: 25,
+  defence: 25,
+  health: 62.5,
+  name: 'aaa',
+  type: 'Undead',
+  death: false
+};
+
+const exepted2 = {
+  attack: 25,
+  defence: 25,
+  health: 0,
+  name: 'aaa',
+  type: 'Undead',
+  death: true
+};
+
+describe('Character', () => {
+  test('should get damage 50 points', () => {
+    const hero = new Character('aaa', 'Undead');
+    hero.damage(50);
+    expect(hero).toEqual(exepted1);
+  });
+
+  test('should get damage 500 points. Fatality!', () => {
+    const hero = new Character('aaa', 'Undead');
+    hero.damage(200);
+    hero.damage(300);
+    expect(hero).toEqual(exepted2);
+  });
+});
---
+++ src/js/app.js
@@ -0,0 +1,22 @@
+export default function Character(name, type) {
+  this.name = name;
+  this.type = type;
+  this.health = 100;
+  this.attack = 25;
+  this.defence = 25;
+  this.death = false;
+}
+
+Character.prototype.damage = function (points) {
+  const damage = points * (1 - this.defence / 100);
+
+  if (damage >= this.health) {
+    this.health = 0;
+  } else {
+    this.health -= damage;
+  }
+
+  if (this.health === 0) {
+    this.death = true;
+  }
+};
